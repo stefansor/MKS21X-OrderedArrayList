d4ad64565d52f62d12ce2a59a930851ee420305e
stefansor <ssorobay00@stuy.edu>
2018-11-02 10:59:03 -0400

    have yet to test

---
+++ OrderedArrayList.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
+	public OrderedArrayList(){
+		super();
+	}
+	public OrderedArrayList(int capacity){
+		super(capacity);
+	}
+	public boolean add(T element){
+		for(int i = 0; i<this.size(); i++){
+			if(this.get(i).compareTo(element) < 0){
+				super.add(i, element);
+			}
+		}
+		return true;
+	}
+	public void add(int index, T element){
+		this.add(element);
+	}
+	public T set(int index, T element){
+		T b;
+		b = this.get(index);
+		this.remove(index);
+		this.add(element);
+		return b;
+	}
+}
